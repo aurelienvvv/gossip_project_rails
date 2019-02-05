80fe5afbc6790246592e3b56b17869ba75f33fb5
Aurelien Verschueren <aurelienverschueren@gmail.com>
2019-02-05 17:30:24 +0100

    first

--- app/controllers/dynamics_pages_controller.rb
+++ app/controllers/dynamics_pages_controller.rb
@@ -6,9 +6,11 @@ class DynamicsPagesController < ApplicationController
   def gossip
     @gossip = Gossip.find_by(id: params[:gossip_id])
     @author_gossip = User.find(@gossip.user_id).first_name
+    @author_gossip_id = User.find(@gossip.user_id)
   end
 
   def user
-    @author = params[:author_id]
+    @author = User.find_by(id: params[:author_id])
+    @author_city = City.find(@author.city_id).name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'author/:author_id', to: 'dynamics_pages#user'
+  get 'author/:author_id', to: 'dynamics_pages#user', as: "author"
 
   get '/', to: 'dynamics_pages#welcome', as: 'welcome'
 
-  get '/gossip/:gossip_id', to: 'dynamics_pages#gossip'
+  get '/gossip/:gossip_id', to: 'dynamics_pages#gossip', as: "gossip"
 
   get 'static_page/contact', to: 'static_page#contact'
   
